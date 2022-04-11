a81689a60ab6ac5a77f6f83f895cb14232de520b
cseltz <chase.seltz21@gmail.com>
2022-04-11 15:06:43 -0700

    Add options for size and color change. Improved color picker function

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const gridContainer = document.getElementById('grid-container');
+const radioSizeSmall = document.getElementById('radio-size-small');
+const radioSizeMed = document.getElementById('radio-size-med');
+const radioSizeLarge = document.getElementById('radio-size-large');
+const radioColorSmall = document.getElementById('radio-color-small');
+const radioColorMed = document.getElementById('radio-color-med');
+const radioColorLarge = document.getElementById('radio-color-large');
 
-let numDivs = 121;
+const smallNumDivs = 64;
+const medNumDivs = 121;
+const largeNumDivs = 225;
+
+let numDivs = medNumDivs;
+let generatedDivs = []; // When divs are created they will be placed in this array
 
 initDivs(numDivs);
-
+radioSizeMed.checked = true; // Default value
+radioColorMed.checked = true; // Default value
 
 let r = Math.floor(Math.random() * 255);
 let g = Math.floor(Math.random() * 255);
@@ -13,12 +25,23 @@ let gUp = false;
 let bUp = true;
 
 
+radioSizeSmall.addEventListener('change', function() {
+    clearGeneratedDivs();
+    initDivs(smallNumDivs);
+});
 
+radioSizeMed.addEventListener('change', function() {
+    clearGeneratedDivs();
+    initDivs(medNumDivs);
+});
 
-
+radioSizeLarge.addEventListener('change', function() {
+    clearGeneratedDivs();
+    initDivs(largeNumDivs);
+});
 
 function initDivs(numberOfDivs) {
-    const divSize = gridContainer.clientHeight / Math.sqrt(numDivs);
+    const divSize = gridContainer.clientHeight / Math.sqrt(numberOfDivs);
     for (let i = 0; i < numberOfDivs; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-div');
@@ -26,16 +49,43 @@ function initDivs(numberOfDivs) {
         div.style.height = `${divSize}px`;
         div.style.border = '1px solid gray';
         div.addEventListener('pointerenter', function() {
-            updateRGB(10, 20, 10);
+            updateRGB(15, 10, 20);
             div.style.backgroundColor = `rgb(${r},${g},${b})`;
         })
+        generatedDivs.push(div);
         gridContainer.appendChild(div);
     }
 }
 
 
+function clearGeneratedDivs() {
+    for (let i = 0; i < generatedDivs.length; i++) {
+        generatedDivs[i].remove();
+    }
+    generatedDivs = [];
+}
 
-function updateRGB(rDelta, gDelta, bDelta) {
+
+
+function updateRGB(redDelta, greenDelta, blueDelta) {
+    let rDelta;
+    let gDelta;
+    let bDelta;
+
+    if (radioColorSmall.checked) {
+        rDelta = Math.floor(redDelta / 5);
+        gDelta = Math.floor(greenDelta / 5);
+        bDelta = Math.floor(blueDelta / 5);
+    } else if (radioColorMed.checked) {
+        rDelta = redDelta;
+        gDelta = greenDelta;
+        bDelta = blueDelta;
+    } else if (radioColorLarge.checked) {
+        rDelta = redDelta * 2;
+        gDelta = greenDelta * 2;
+        bDelta = blueDelta * 2;
+    }
+
     if (rUp) {
         if (r < 255 - rDelta) {
             r += rDelta;
